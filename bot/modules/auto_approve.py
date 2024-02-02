a953d603e3b9de42acd4020c2cdf3418a4705499
r4ndomusers <e.luckm4n@gmail.com>
2024-02-03 05:12:24 +0700

    Send message approval to user

--- bot/modules/auto_approve.py
+++ bot/modules/auto_approve.py
@@ -1,5 +1,5 @@
-from aiofiles.os import makedirs
-from asyncio import gather
+from aiofiles.os import makedirs, remove, path as aiopath
+from asyncio import gather, sleep
 from os import path as ospath
 from pyrogram import Client
 from pyrogram.filters import group, channel, chat
@@ -14,16 +14,16 @@ from bot.helpers.utils import update_users
 async def on_join_request(client: Client, message: Message):
     user_id = message.from_user.id
     chat = await client.get_chat(message.chat.id)
-    text = 'Congratulation, your request was approved!\n\n'
-    text += '<b>CHAT INFO</b>\n'
+    text = '<b>CHAT INFO</b>\n'
     text += f'<b>Title:</b> {chat.title}\n'
     text += f'<b>ID:</b> <code>{chat.id}</code>\n'
     text += f'<b>Type:</b> {chat.type.name.title()}\n'
     text += f'<b>Member:</b> {chat.members_count}\n'
     text += f'<b>Data Center:</b> {chat.dc_id or "Unknown"}\n'
-    text += f'<b>Protect Content:</b> {chat.has_protected_content}'
+    text += f'<b>Protect Content:</b> {chat.has_protected_content}\n\n'
+    text += 'Congratulation, your request was approved!'
 
-    image = None
+    image = ''
     if chat.photo:
         await makedirs('images', exist_ok=True)
         try:
@@ -31,21 +31,25 @@ async def on_join_request(client: Client, message: Message):
         except:
             pass
 
+    await sendPhotoTo(text, user_id, image) if image else sendCustom(text, user_id)
+    await sleep(1)
     await gather(update_users(user_id, 'tag', message.from_user.mention),
                  client.approve_chat_join_request(message.chat.id, user_id))
 
-    await sendPhotoTo(text, user_id, image) if image else sendCustom(text, user_id)
-
     if LOG_CHANNEL:
         user = await client.get_users(user_id)
-        image = None
+        image = ''
         if user.photo:
             try:
                 image = await client.download_media(user.photo.big_file_id, ospath.join('images', f'{user.id}.jpg'))
             except:
                 pass
         text = f'{message.from_user.mention} <code>({user_id})</code>, approved to join <b>{chat.title}</b> ({chat.type.name.title()})'
-        await sendPhotoTo(text, LOG_CHANNEL, image) if image else sendCustom(text, LOG_CHANNEL)
+        if await aiopath.isfile(image):
+            await sendPhotoTo(text, LOG_CHANNEL, image)
+            await remove(image)
+        else:
+            await sendCustom(text, LOG_CHANNEL)
 
 
 bot.add_handler(ChatJoinRequestHandler(on_join_request, (group | channel) & chat(list(CHAT_IDS)) if CHAT_IDS else (group | channel)))
